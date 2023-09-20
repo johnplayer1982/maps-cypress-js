18a222146206569d54af58228bcdd5a422fc6b1c
John Player <johnplayer1982@msn.com>
2023-09-20 07:18:28 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+```
+yarn add cypress --dev
+yarn run cypress open
+```
---
+++ cypress/e2e/example.cy.js
@@ -0,0 +1,8 @@
+describe('description', () => {
+    beforeEach(() => {
+
+    })
+    it('does_something', () => {
+        cy.visit('https://www.maps.org.uk')
+    })
+})
